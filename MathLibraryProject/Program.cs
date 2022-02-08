fc50a34d12b59254815c228397f6b76541760e99
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-08 15:56:52 -0500

    Add project files.

---
+++ MathLibraryProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MathLibraryProject {
+    class Program {
+        static void Main(string[] args) {
+            var a = Math.Add("1", "2"); //var has to be assigned however can be used in place of specific types
+        }
+    }
+}
